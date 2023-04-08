79d16e26d5f654b6c67de04fc6208a97c201c0cf
Lexuil <ojumanab@unal.edu.co>
2023-04-08 16:18:17 -0500

    move fonts folder

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import './fonts/Poppins-Regular.ttf'
+import './fonts/Poppins-Bold.ttf'
+import './fonts/Poppins-ExtraBold.ttf'
+import './fonts/Poppins-ExtraBoldItalic.ttf'
+import './fonts/Poppins-Italic.ttf'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
